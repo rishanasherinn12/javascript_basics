d101cb368f4342a5ce5a2f3d208e9f87ba67c947
Rishana Sherin <rishanasherinn12@gmail.com>
2026-04-05 22:37:44 +0530

    Add files via upload

---
+++ typecasting.js
@@ -0,0 +1,16 @@
+// into string
+let a = 100;
+console.log(a);
+console.log(typeof a);
+b=String(a);
+console.log(b);
+console.log(typeof b);
+
+
+let c = true;
+console.log(c);
+console.log(typeof c);
+d=String(c);
+console.log(d);
+console.log(typeof d);
+
