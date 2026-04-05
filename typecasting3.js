8b5953859d34a93dbcc82adae899bd7713ea7f6a
Rishana Sherin <rishanasherinn12@gmail.com>
2026-04-06 01:20:35 +0530

    Add files via upload

---
+++ typecasting3.js
@@ -0,0 +1,19 @@
+// // into boolean
+// let a=123
+// console.log(a)
+// console.log(typeof a)
+// console.log(Boolean(a))
+
+
+// let a=Infinity
+// console.log(a)
+// console.log(typeof a)
+// console.log(Boolean(a))
+
+
+// prints NaN
+let a="hello"/2
+console.log(a)
+console.log(typeof a)
+
+console.log(Boolean(a))
