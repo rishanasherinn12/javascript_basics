ea1a1044a8fa3360d0754369f75e72dbdf15afea
Rishana Sherin <rishanasherinn12@gmail.com>
2026-04-05 22:38:06 +0530

    Add files via upload

---
+++ typecasting2.js
@@ -0,0 +1,18 @@
+// into number
+
+// let a ="123tap"
+// console.log(a)
+// console.log(typeof a)
+// b=Number(a);
+// console.log(b)
+// console.log(typeof b)
+
+let c =" 1 23"
+b=parseInt(c);
+console.log(b)
+console.log(typeof b)
+
+let d =" 123"
+b=parseInt(d);
+console.log(b)
+console.log(typeof b)
